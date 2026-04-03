a4c356d4c7df0ae6eebe75181462fed1a42a3c87
Mdednnjya <narayanmade578@gmail.com>
2026-04-04 05:14:53 +0700

    fix: resolve silent failure, enforce RESTful array response, and standardize logging

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"core-ticketing-engine/internal/handler"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -16,7 +15,7 @@ func main() {
 	mux.HandleFunc("/health", handler.HealthCheckHandler)
 	mux.HandleFunc("/api/tickets", handler.TicketsHandler)
 
-	fmt.Printf("Server is Running on Port %s", port)
+	log.Printf("Server is Running on Port %s \n", port)
 	err := http.ListenAndServe(port, mux)
 
 	if err != nil {
--- internal/handler/ticket_handler.go
+++ internal/handler/ticket_handler.go
@@ -8,16 +8,25 @@ import (
 	
 
 func TicketsHandler(w http.ResponseWriter, r *http.Request) {
-	ticket01 := entity.Ticket{
-		ID: 01, 
-		EventName: "Coldplay",
-		Price: 150,
+	ticket01 := []entity.Ticket{
+		{
+			ID: 1, 
+			EventName: "Coldplay",
+			Price: 150,
+		},
+		{
+			ID: 2,
+			EventName: "Coldplay JKT",
+			Price: 175,
+		},
 	}
 
 	data, err := json.Marshal(ticket01)
 
 	if err != nil {
-		// edge case logic
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("{error: Internal Server Error!}"))
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(data)
